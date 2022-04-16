853949a1ff2c1765ce3e288d6fde333762384447
Sebastian Tirsén <sebastian.tirsen@gmail.com>
2022-04-16 12:26:46 +0000

    Added furnitures app and models.

---
+++ furnitures/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+
+
+class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.friendly_name
+
+    def get_fiendly_name(self):
+        return self.friendly_name
+
+
+class Furniture(models.Model):
+    category = models.ForeignKey(
+        'Category', null=True, blank=True, on_delete=models.SET_NULL)
+    brand = models.CharField(max_length=254)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    dimensions = models.CharField(max_length=254)
+    material = models.CharField(max_length=254)
+    Production = models.CharField(max_length=254)
+
+    choices = (
+            ('1', 'Like New'),
+            ('2', 'Good'),
+            ('2', 'Worn'),
+        )
+  
+    condition = models.CharField(max_length=300, null=True, blank=True, choices = choices)
+
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
