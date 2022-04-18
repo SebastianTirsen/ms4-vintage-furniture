5685581a1a34bc5e016c53b1f3d557eae7070990
Sebastian Tirsén <sebastian.tirsen@gmail.com>
2022-04-18 08:14:20 +0000

    Added functionality for sorting the furniture.

--- furnitures/views.py
+++ furnitures/views.py
@@ -10,8 +10,23 @@ def all_furniture(request):
     furnitures = Furniture.objects.all()
     query = None
     categories = None
+    sort = None
+    direction = None
 
     if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                furnitures = furnitures.annotate(lower_name('name'))
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            furnitures = furnitures.order_by(sortkey)
+
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             furnitures = furnitures.filter(category__name__in=categories)
@@ -25,11 +40,14 @@ def all_furniture(request):
 
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             furnitures = furnitures.filter(queries)
+    
+    current_sorting = f'{sort}_{direction}'
 
     context = {
         'furnitures': furnitures,
         'search_term': query,
         'current_categories': categories,
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'furnitures/furnitures.html', context)
