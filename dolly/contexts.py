66f6f8277785e44ff57de4f8cb14a800dd44a0ea
Sebastian Tirsén <sebastian.tirsen@gmail.com>
2022-04-20 19:45:29 +0000

    Added shopping dolly template and added info, images etc.

--- dolly/contexts.py
+++ dolly/contexts.py
@@ -1,11 +1,24 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from furnitures.models import Furniture
 
 def dolly_contents(request):
 
     dolly_items = []
     total = 0
     furniture_count = 0
+    dolly = request.session.get('dolly', {})
+
+    for item_id, quantity in dolly.items():
+        furniture = get_object_or_404(Furniture, pk=item_id)
+        total += quantity * furniture.price
+        furniture_count += quantity
+        dolly_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'furniture': furniture,
+        })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
